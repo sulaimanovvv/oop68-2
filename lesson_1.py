906d1314cf974c40ed2b1c611567aa424e34f6ff
sulaimanovvv <sulaimanoff.imran@yandex.com>
2026-07-04 13:24:28 +0600

    first commit

---
+++ lesson_1.py
@@ -0,0 +1,20 @@
+class Hero:
+    # Конструктор класса
+    def __init__(self, name, hp, lvl):
+        # Атрибуты экземпляра класса
+        self.hero_name = name
+        self.hero_hp = hp
+        self.hero_lvl = lvl
+
+    # Метод класса
+    def base_action(self):
+        return f"this base action {self.hero_name}"
+
+
+# Обьект(Экземпляр класса)
+kirito = Hero("Кирито", 1000, 100)
+asuna = Hero("Asuna", 1200, 110)
+
+print(kirito.hero_name)
+print(asuna.hero_name)
+print(kirito.base_action())
